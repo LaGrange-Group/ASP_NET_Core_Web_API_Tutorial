23a7c797172118a1b049d727e64e5602f2c54dbd
David La Grange <davidnlagrange@hotmail.com>
2021-09-03 11:32:50 -0500

    Added Extensions Folder + Created Static ServiceExtension class + Configured SQL Configuration + Register SQL Configuration in Configure Services

---
+++ ASPNETCoreWebAPIJWTTutorial/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ASPNETCoreWebAPIJWTTutorial.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCoreWebAPIJWTTutorial.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<ApplicationDbContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+    }
+}
--- ASPNETCoreWebAPIJWTTutorial/Startup.cs
+++ ASPNETCoreWebAPIJWTTutorial/Startup.cs
@@ -1,3 +1,4 @@
+using ASPNETCoreWebAPIJWTTutorial.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace ASPNETCoreWebAPIJWTTutorial
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.ConfigureSqlContext(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
