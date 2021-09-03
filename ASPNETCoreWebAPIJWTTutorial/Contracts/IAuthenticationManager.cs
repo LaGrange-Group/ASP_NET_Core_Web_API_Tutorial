ef1339adec4abfdd37721d65b4490589da6abec9
David La Grange <davidnlagrange@hotmail.com>
2021-09-03 15:02:14 -0500

    Created Contracts Folder + Created IAuthenticationManager Interface for the AuthenticationManager Class

---
+++ ASPNETCoreWebAPIJWTTutorial/Contracts/IAuthenticationManager.cs
@@ -0,0 +1,11 @@
+﻿using ASPNETCoreWebAPIJWTTutorial.DataTransferObjects;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWebAPIJWTTutorial.Contracts
+{
+    public interface IAuthenticationManager
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
+    }
+}
